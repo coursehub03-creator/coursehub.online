08d1e5b4f5821d550bc59ba2f3eb89adff7e722a
coursehub03-creator <coursehub03@gmail.com>
2026-01-19 16:28:43 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="header.css">
+
 ```javascript
 const {
   auth,
@@ -59,4 +59,4 @@ verifyCode.onclick = async () => {
   window.location.href = "index.html";
 };
 ```
-<link rel="stylesheet" href="footer.css">
+
