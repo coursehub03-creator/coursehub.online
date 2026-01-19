f822845dac237f068a4039fecf9bfdb7cf9ad349
coursehub03-creator <coursehub03@gmail.com>
2026-01-19 15:47:40 +0100

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,7 @@
+// تحديث السنة تلقائياً
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("year");
+  if(yearSpan){
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
