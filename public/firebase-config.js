1836a346e21a8ff114e69ef4f3464ce334703b97
coursehub03-creator <coursehub03@gmail.com>
2026-01-20 19:52:02 +0100

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -4,7 +4,6 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged }
     from "https://www.gstatic.com/firebasejs/10.16.0/firebase-auth.js";
 import { getFirestore, doc, getDoc, setDoc, updateDoc, arrayUnion } 
     from "https://www.gstatic.com/firebasejs/10.16.0/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.16.0/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCagdZU_eAHebBGCmG5W4FFTcDZIH4wOp0",
@@ -17,7 +16,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
