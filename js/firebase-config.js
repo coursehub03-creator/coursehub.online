c68a145e1594b39d86daa9fabe711b74e5af4d9e
coursehub03-creator <coursehub03@gmail.com>
2026-01-22 19:06:27 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,33 @@
 // js/firebase-config.js
+// ====================
+// إعداد Firebase الأساسي لجميع الصفحات
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
 
+// إعدادات Firebase لمشروعك
 const firebaseConfig = {
   apiKey: "AIzaSyCagdZU_eAHebBGCmG5W4FFTcDZIH4wOp0",
   authDomain: "coursehub-23ed2.firebaseapp.com",
   projectId: "coursehub-23ed2",
-  storageBucket: "coursehub-23ed2.appspot.com",
+  storageBucket: "coursehub-23ed2.appspot.com", // صححت هنا
   messagingSenderId: "367073521017",
-  appId: "1:367073521017:web:67f5fd3be4c6407247d3a8"
+  appId: "1:367073521017:web:67f5fd3be4c6407247d3a8",
+  measurementId: "G-NJ6E39V9NW" // لو حاب تستخدم Analytics
 };
 
+// تهيئة Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 
+// Authentication
+export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({ prompt: "select_account" });
 
+// Firestore
 export const db = getFirestore(app);
+
+// Analytics (اختياري)
+export const analytics = getAnalytics(app);
