d7bc5d98dfc0aedea24567a1d896504a40d850ac
coursehub03-creator <coursehub03@gmail.com>
2026-01-22 20:18:00 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,6 @@
+
+
 // js/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
 
 // إعدادات Firebase لمشروعك
 const firebaseConfig = {
@@ -15,21 +13,8 @@ const firebaseConfig = {
   measurementId: "G-NJ6E39V9NW"
 };
 
-// تهيئة Firebase
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// Authentication
-export const auth = getAuth(app);
-
-// Google Sign-In
-export const googleProvider = new GoogleAuthProvider();
-googleProvider.setCustomParameters({ prompt: "select_account" });
-
-// Client ID مخصص (Google OAuth Web Client ID)
-export const CLIENT_ID = "367073521017-3ar9b55sgp9ud352l0qod946th1t51q9.apps.googleusercontent.com";
-
-// Firestore
-export const db = getFirestore(app);
-
-// Analytics (اختياري)
-export const analytics = getAnalytics(app);
+// Firebase Auth
+const auth = firebase.auth();
