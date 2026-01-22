11bc8425663581a1da2e7d4498f0ee9d911722f2
coursehub03-creator <coursehub03@gmail.com>
2026-01-22 15:52:25 +0100

    Update firebase-config.js

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,8 +1,6 @@
 // js/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.16.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.16.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.16.0/firebase-firestore.js";
 
+// إعداد Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCagdZU_eAHebBGCmG5W4FFTcDZIH4wOp0",
   authDomain: "coursehub-23ed2.firebaseapp.com",
@@ -12,13 +10,13 @@ const firebaseConfig = {
   appId: "1:367073521017:web:67f5fd3be4c6407247d3a8"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase (نسخة التوافق - compat)
+firebase.initializeApp(firebaseConfig);
 
 // Auth & Firestore
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const auth = firebase.auth();
+const db = firebase.firestore();
 
-// Google provider
-export const googleProvider = new GoogleAuthProvider();
+// Google Provider
+const googleProvider = new firebase.auth.GoogleAuthProvider();
 googleProvider.setCustomParameters({ prompt: "select_account" });
