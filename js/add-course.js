85652e5939b5df020d022113e18721f36ec5928a
coursehub03-creator <coursehub03@gmail.com>
2026-01-23 13:46:59 +0100

    Update add-course.js

--- js/add-course.js
+++ js/add-course.js
@@ -10,12 +10,13 @@ import {
 // إعداد Firebase
 // -----------------------------
 const firebaseConfig = {
-  apiKey: "AIzaSyCagdZU_eAHebBGCmG5W4FFTcDZIH4wOp0",
+  apiKey: "AIzaSyDV9DaGphy6cyNt1UEITkQDlTim1w8vm8M",
   authDomain: "coursehub-23ed2.firebaseapp.com",
   projectId: "coursehub-23ed2",
   storageBucket: "coursehub-23ed2.firebasestorage.app",
   messagingSenderId: "367073521017",
-  appId: "1:367073521017:web:67f5fd3be4c6407247d3a8"
+  appId: "1:367073521017:web:67f5fd3be4c6407247d3a8",
+  measurementId: "G-NJ6E39V9NW"
 };
 
 const app = initializeApp(firebaseConfig);
