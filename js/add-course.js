2cc9a7b0bcca0da5dce9d68d60fde71817ccd4ec
coursehub03-creator <coursehub03@gmail.com>
2026-01-23 17:01:40 +0100

    Update add-course.js

--- js/add-course.js
+++ js/add-course.js
@@ -10,7 +10,7 @@ import {
 // إعداد Firebase
 // -----------------------------
 const firebaseConfig = {
- apiKey: "AIzaSyDTW6hv7_PqPUb8NytYXVkkrVwwE9ACY0I",
+  apiKey: "AIzaSyDV9DaGphy6cyNt1UEITkQDlTim1w8vm8M",
   authDomain: "coursehub-23ed2.firebaseapp.com",
   projectId: "coursehub-23ed2",
   storageBucket: "coursehub-23ed2.firebasestorage.app",
