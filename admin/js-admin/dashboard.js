1bf03d61c375951d1bf1293694ce9e1ffbe36fe8
coursehub03-creator <coursehub03@gmail.com>
2026-01-24 14:41:18 +0100

    Update dashboard.js

--- admin/js-admin/dashboard.js
+++ admin/js-admin/dashboard.js
@@ -1,6 +1,6 @@
 // dashboard.js
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.16.0/firebase-firestore.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.16.0/firebase-auth.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const auth = getAuth();
 const db = getFirestore();
