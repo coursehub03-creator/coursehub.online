e5e0825ab956decd96c283f32e14802161a4ef79
coursehub03-creator <coursehub03@gmail.com>
2026-01-24 22:10:59 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDa84fRquyZah629wkTZACFVVZ7Gmnk1MY",
   authDomain: "coursehub-23ed2.firebaseapp.com",
   projectId: "coursehub-23ed2",
-  storageBucket: "coursehub-23ed2.appspot.com",
+  storageBucket: "coursehub-23ed2.firebasestorage.app",
   messagingSenderId: "367073521017",
   appId: "1:367073521017:web:67f5fd3be4c6407247d3a8",
   measurementId: "G-NJ6E39V9NW"
